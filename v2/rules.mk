fcba20fb437d79f711650b5140c3fb646a1da2f1
ickule <117572745+ickule@users.noreply.github.com>
2023-05-06 11:56:30 +0200

    Initial commit.

---
+++ v2/rules.mk
@@ -0,0 +1,6 @@
+# Build Options
+#   change yes to no to disable
+#
+VIA_ENABLE = no			# Enable VIA firmware
+BACKLIGHT_ENABLE = no	# Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no	# Enable keyboard RGB underglow
