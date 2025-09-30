b2cc48678632337a9c8f30d6cd1b2e29ab7c84c4
gsegt <git@gsegt.eu>
2025-09-30 20:43:10 +0200

    feat: add support for nkro

--- dz60/keymaps/gsegt/keymap.c
+++ dz60/keymaps/gsegt/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     QK_BOOT,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_F13,   KC_F14,
     _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_VOLU,  KC_PSCR,  KC_F15,   KC_F16,   KC_F17,
     _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_VOLD,  _______,  _______,  _______,
-    _______,            _______,  _______,  _______,  _______,  _______,  _______,  KC_MUTE,  KC_MPRV,  KC_MNXT,  _______,            KC_PGUP,  _______,
+    _______,            _______,  _______,  _______,  _______,  _______,  NK_TOGG,  KC_MUTE,  KC_MPRV,  KC_MNXT,  _______,            KC_PGUP,  _______,
     _______,  _______,  _______,                                KC_MPLY,                                A(KC_F4), KC_HOME,            KC_PGDN,  KC_END
   )
 };
